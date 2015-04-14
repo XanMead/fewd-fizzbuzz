97ef01af39ed5d49be844d5b2ecd6b56d01ad7a4
XanMead <x.m.mead@gmail.com>
2015-04-14 13:00:31 -0400

    refactored fizzbuzz with modularized functions

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,20 +1,40 @@
 $(document).ready(function() {
-	var spoken;
-	var word;
-	for (var i = 1; i <= 100; i++) {
-		spoken = false;
-		word = "";
-		if (i % 3 == 0) {
-			word += "fizz";
-			spoken = true;
+	$('#go').on('click', function() {
+		var nbr = $('#numBox').val();
+		nbr = parseInt(nbr);
+		if (nbr > 0) {
+			clear('console');
+			fizzbuzzer(nbr);
 		}
-		if (i % 5 == 0) {
-			word += "buzz";
-			spoken = true;
+		else {
+			alert('Please enter a positive integer.');
 		}
-		if (!spoken) {
-			word += i;
-		}
-		$("body").append("<p>" + word + "</p>");
-	}
+		$('#numBox').val('');
+	});
 });
+
+function clear(divId) {
+	$('#' + divId).children().remove();
+}
+
+function printLine(str) {
+	console.log(str);
+	var n = document.createElement('p'),
+	    t = document.createTextNode(str);
+
+	n.appendChild(t);
+	document.getElementById('console').appendChild(n);
+}
+
+function fizzbuzzer(n) {
+	for (var i = 1; i <= n; i++) {
+		printLine(fizzbuzz(i));
+	}
+}
+
+function fizzbuzz(x) {
+	var msg = '';
+	if (x % 3 == 0) {msg += 'fizz';}
+	if (x % 5 == 0) {msg += 'buzz';}
+	return msg || x;
+}
